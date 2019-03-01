c9a6db1208ddccfd6d3e57428bb5d819f3eb5e08
baolong301 <lelacbinh3107@gmail.com>
2019-03-02 01:23:34 +0700

    finish index

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,23 @@
 // Main
-
+var width = $(window).width();
 $(document).ready(function() {
     // Show scrollTop
     console.log($(window).scrollTop());
-    $(window).scroll(function() {
-        var scrolltop = $(window).scrollTop();
-        console.log(scrolltop);
-        if (scrolltop > 10) {
-            $('header ul li a').removeClass('white');
-            $('header').css({ 'background': 'white', 'color': 'black!important', 'box-shadow': '0 0.5px 15px 1px black' });
-        } else {
-            $('header ul li a').addClass('white');
-            $('header').css({ 'background': 'initial', 'color': 'initial', 'box-shadow': 'initial' });
-        }
-    });
+    if (width >= 768) {
+        $(window).scroll(function() {
+            var scrolltop = $(window).scrollTop();
+            // console.log(scrolltop);
+            if (scrolltop > 10) {
+                $('header ul li a').removeClass('white');
+                $('header').css({ 'background': 'white', 'color': 'black!important', 'box-shadow': '0 0.5px 15px 1px black' });
+            } else {
+                $('header ul li a').addClass('white');
+                $('header').css({ 'background': 'initial', 'color': 'initial', 'box-shadow': 'initial' });
+            }
+        });
+    } else {
+        $('header').css({ 'background': 'white', 'box-shadow': '0 0.5px 15px 1px black', 'position': 'initial' });
+    }
     // Phần Slider
     $('.home-slider .owl-carousel').owlCarousel({
         items: 1,
@@ -24,9 +28,22 @@ $(document).ready(function() {
     // Phần Clients
     $('.home-clients .owl-carousel').owlCarousel({
         items: 1,
-        nav: true,
+        nav: false,
         dots: false,
-        navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+        loop: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+    });
+    // Phần Partners
+    $('.home-partners .owl-carousel').owlCarousel({
+        items: 1,
+        loop: true,
+        nav: false,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 1000,
         responsive: {
             // breakpoint from 480 up
             480: {
